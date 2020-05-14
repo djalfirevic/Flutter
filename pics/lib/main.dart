d25f6e7aceae0646384f2a26611cee6b59e46871
Djuro Alfirevic <djalfirevic@gmail.com>
2020-05-14 11:02:10 +0200

    app.dart finished

--- pics/lib/main.dart
+++ pics/lib/main.dart
@@ -1,17 +1,6 @@
 import 'package:flutter/material.dart';
+import 'src/app.dart';
 
 void main() {
-  var app = MaterialApp(
-    home: Scaffold(
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () {},
-      ),
-      appBar: AppBar(
-        title: Text("Let's see some images!"),
-      ),
-    ),
-  );
-
-  runApp(app);
+  runApp(App());
 }
--- pics/lib/src/app.dart
+++ pics/lib/src/app.dart
@@ -1,1 +1,17 @@
 import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  Widget build(context) {
+    return MaterialApp(
+      home: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: () {},
+        ),
+        appBar: AppBar(
+          title: Text("Let's see some images!"),
+        ),
+      ),
+    );
+  }
+}
