bee9d62ff614fca90ad3e1b8fa45735947b8c7f1
Djuro Alfirevic <djalfirevic@gmail.com>
2020-05-15 10:33:30 +0200

    Added state to app.dart

--- pics/lib/src/app.dart
+++ pics/lib/src/app.dart
@@ -1,12 +1,26 @@
 import 'package:flutter/material.dart';
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return AppState();
+  }
+}
+
+class AppState extends State<App> {
+  int counter = 0;
+
   Widget build(context) {
     return MaterialApp(
       home: Scaffold(
+        body: Text('$counter'),
         floatingActionButton: FloatingActionButton(
           child: Icon(Icons.add),
-          onPressed: () {},
+          onPressed: () {
+            setState(() {
+              counter += 1;
+            });
+          },
         ),
         appBar: AppBar(
           title: Text("Let's see some images!"),
