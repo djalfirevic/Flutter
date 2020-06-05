f1ec9b36a299a5bb53073f3a1a964dffcd3673d5
Djuro Alfirevic <djalfirevic@gmail.com>
2020-06-05 08:39:43 +0200

    login_bloc project added

--- login_bloc/lib/src/app.dart
+++ login_bloc/lib/src/app.dart
@@ -1,16 +1,12 @@
 import 'package:flutter/material.dart';
 import 'screens/login_screen.dart';
-import 'blocs/provider.dart';
 
 class App extends StatelessWidget {
   build(context) {
-    return Provider(
-      child: MaterialApp(
+    return MaterialApp(
         title: 'Log Me In',
         home: Scaffold(
           body: LoginScreen(),
-        ),
-      ),
-    );
+        ));
   }
 }
